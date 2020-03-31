c670a3ede588cc8fe5bd9260fe5a6b33bd759842
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-03-31 23:42:40 +0100

    Completely removed form validation

--- miels.js
+++ miels.js
@@ -2,13 +2,6 @@ function openForm() {
   document.getElementById("reserveForm").style.display = "block";
 }
 
-// function validateForm() {
-//   var x = document.forms["reservation-form"]["cName"].value;
-//   if (x == "") {
-//     alert("Please provide your name");
-//     return false;
-//   }
-// }
 
 
 
