01ceae4bbd6115c075e97e7b9c12667e4524026f
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-03-31 23:38:46 +0100

    Revert "Removed form validation in JS"
    This reverts commit e5333d4659ab0db189bec7d78f10902146c323aa.

--- miels.js
+++ miels.js
@@ -2,13 +2,13 @@ function openForm() {
   document.getElementById("reserveForm").style.display = "block";
 }
 
-// function validateForm() {
-//   var x = document.forms["reservation-form"]["cName"].value;
-//   if (x == "") {
-//     alert("Please provide your name");
-//     return false;
-//   }
-// }
+function validateForm() {
+  var x = document.forms["reservation-form"]["cName"].value;
+  if (x == "") {
+    alert("Please provide your name");
+    return false;
+  }
+}
 
 
 
